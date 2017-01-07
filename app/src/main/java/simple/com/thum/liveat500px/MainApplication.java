89941022aba9406a8f4db2f40ff905f90cc8df7b
Sobleach <suchat.nu@gmail.com>
2017-01-07 17:40:06 +0700

    125-131 Lecture 126 - Create a New Project and Structure with MVC Lecture 127 - Colorize the ActionBar: Material Design Color Pallette Lecture 128 - Drawer Menu & Hamburger Icon UX Overview Lecture 129 - Implement Drawer Menu Lecture 130 - Implement Hamburger Icon Lecture 131 - Goodbye ActionBar, Say Hi to Toolbar !

--- app/src/main/java/simple/com/thum/liveat500px/MainApplication.java
+++ app/src/main/java/simple/com/thum/liveat500px/MainApplication.java
@@ -3,6 +3,8 @@ package simple.com.thum.liveat500px;
 
 import android.app.Application;
 
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
 /**
  * Created by Thum on 7/1/2560.
  */
@@ -13,6 +15,7 @@ public class MainApplication extends Application {
         super.onCreate();
 
         // Initialize thing(s) here
+        Contextor.getInstance().init(getApplicationContext());
     }
 
     @Override
