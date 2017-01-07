e6d6d98bba1524b5e8aa29a7236604614066352a
Sobleach <suchat.nu@gmail.com>
2017-01-07 13:44:36 +0700

    Initial commit

---
+++ app/src/main/java/simple/com/thum/liveat500px/MainApplication.java
@@ -0,0 +1,22 @@
+package simple.com.thum.liveat500px;
+
+
+import android.app.Application;
+
+/**
+ * Created by Thum on 7/1/2560.
+ */
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize thing(s) here
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
---
+++ app/src/main/java/simple/com/thum/liveat500px/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package simple.com.thum.liveat500px.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import simple.com.thum.liveat500px.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
